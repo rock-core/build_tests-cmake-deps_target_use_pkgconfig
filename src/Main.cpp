5800524d8ba96412b194c5dad2632dc7f132f03c
Sylvain <sylvain.joyeux@tidewise.io>
2020-12-17 16:24:55 -0300

    use the new dependency that is propagated through CMake exports

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <deps_target_use_pkgconfig/Dummy.hpp>
 #include <deps_target_transitive_dependency/Dummy.hpp>
+#include <deps_target_transitive_dependency/DummyCMake.hpp>
 
 int main(int argc, char** argv)
 {
     deps_target_use_pkgconfig::DummyClass dummyClass;
     deps_target_transitive_dependency::DummyClass dummy;
+    deps_target_transitive_dependency::DummyCMake dummyCmake;
 
     dummyClass.welcome();
     dummy.welcome();
